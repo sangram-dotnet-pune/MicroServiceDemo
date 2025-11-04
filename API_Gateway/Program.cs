223821147f215c0abda6596205d1cf3b3149103b
sangram-dotnet-pune <sangramcanva7812@gmail.com>
2025-11-04 15:01:33 +0530

    Add project files.

---
+++ API_Gateway/Program.cs
@@ -0,0 +1,31 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot(); // Register Ocelot services
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true); // Load Ocelot configuration
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+await app.UseOcelot();// Use Ocelot middleware
+
+app.Run();
---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "order one from home", "order two from office" };
+        }
+
+        // GET api/<OrdersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "order for home";
+        }
+
+        // POST api/<OrdersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrdersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrdersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
